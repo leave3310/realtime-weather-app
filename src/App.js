d784419d6219ed253daa5c866d9690da0be6f17b
leave3310 <sampisam3@gmail.com>
2021-04-16 13:29:52 +0800

    use async func to wait api return

--- src/App.js
+++ src/App.js
@@ -137,6 +137,59 @@ const AUTHORIZATION_KEY = 'CWB-A03D04E8-E2FD-42A0-B36C-2D030C9908F8'
 const LOCATION_NAME = '臺北'
 const LOCATION_NAME_FORCAST = '宜蘭縣'
 
+const fetchCurrentWeather = () => {
+  return fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`)
+    .then((response) => response.json())
+    .then((data) => {
+      // 測試資料內容
+      // console.log('data ', data) 
+      const locationData = data.records.location[0]
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, currentElement) => {
+          if (['WDSD', 'TEMP'].includes(currentElement.elementName)) {
+            neededElements[currentElement.elementName] = currentElement.elementValue
+          }
+          return neededElements
+        }, {}
+      )
+
+      return {
+        location: locationData.locationName,
+        windSpeed: weatherElements.WDSD,
+        temperature: weatherElements.TEMP,
+        observationTime: locationData.time.obsTime,
+      }
+    })
+}
+
+const fetchWeatherForecast = () => {
+  return fetch(
+    `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME_FORCAST}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log('data', data)
+      const locationData = data.records.location[0]
+
+      const weatherElements = locationData.weatherElement.reduce(
+        (neededElements, item) => {
+          if (['Wx', 'PoP', 'CI'].includes(item.elementName)) {
+            neededElements[item.elementName] = item.time[0].parameter
+          }
+          return neededElements
+        }, {}
+      )
+
+      return {
+        description: weatherElements.Wx.parameterName,
+        weatherCode: weatherElements.Wx.parameterValue,
+        rainPossibility: weatherElements.PoP.parameterName,
+        comfortability: weatherElements.CI.parameterName
+      }
+
+    })
+
+}
 function App() {
   //console.log('invoke')
   const [currentTheme, setCurrentTheme] = useState('light')
@@ -152,67 +205,21 @@ function App() {
     isLoading: true
   })
   useEffect(() => {
-    //console.log('useEffect')
-    fetchCurrentWeather()
-    fetchWeatherForecast()
+    const fetchData = async () => {
+      setWeatherElement({
+        ...weatherElement,
+        isLoading: true
+      })
+      //等待兩筆資料都好之後再設定
+      const [currentWeather, weatherForecast] = await Promise.all([fetchCurrentWeather(), fetchWeatherForecast()])
+
+      //console.log(currentWeather, weatherForecast)
+      setWeatherElement({ ...currentWeather, ...weatherForecast, isLoading: false })
+    }
+    
+    fetchData()
   }, [])
-  const fetchCurrentWeather = () => {
-    setWeatherElement((preState) => ({ ...preState, isLoading: true }))
-    fetch(`https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0003-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // 測試資料內容
-        // console.log('data ', data) 
-        const locationData = data.records.location[0]
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, currentElement) => {
-            if (['WDSD', 'TEMP'].includes(currentElement.elementName)) {
-              neededElements[currentElement.elementName] = currentElement.elementValue
-            }
-            return neededElements
-          }, {}
-        )
-        //第一種設定weatherElement的方法
-        setWeatherElement({
-          ...weatherElement,
-          location: locationData.locationName,
-          windSpeed: weatherElements.WDSD,
-          temperature: weatherElements.TEMP,
-          observationTime: locationData.time.obsTime,
-          isLoading: false
-        })
-      })
-  }
-  const fetchWeatherForecast = () => {
-    fetch(
-      `https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=${AUTHORIZATION_KEY}&locationName=${LOCATION_NAME_FORCAST}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        //console.log('data', data)
-        const locationData = data.records.location[0]
 
-        const weatherElements = locationData.weatherElement.reduce(
-          (neededElements, item) => {
-            if (['Wx', 'PoP', 'CI'].includes(item.elementName)) {
-              neededElements[item.elementName] = item.time[0].parameter
-            }
-            return neededElements
-          }, {}
-        )
-        //第二種設定weatherElement的方法
-        setWeatherElement(
-          (prevState) => ({
-            ...prevState,
-            description: weatherElements.Wx.parameterName,
-            weatherCode: weatherElements.Wx.parameterValue,
-            rainPossibility: weatherElements.PoP.parameterName,
-            comfortability: weatherElements.CI.parameterName
-          }))
-
-      })
-
-  }
 
   const {
     location,
